071b60610eec55172ec5643019672dba8cd98137
Chih <allenjbrionez@gmail.com>
2023-04-21 01:35:30 -0700

    done js

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
+let pScore = 0;
+let cScore = 0;
+
+function gameOver(num1, num2)
+{
+    if (num1==5)
+    {
+        alert('Congrats! You Win!');
+        
+    }
+    else if (num2==5)
+        alert('Game Over! You Lost!');
+}
+
 function computerTurn(num)
 {
     let randomNum = Math.floor(Math.random () * 3);
     console.log(randomNum);
     
-    if (num>randomNum && num !=0)
-    {
-        document.getElementById('decision').innerHTML = "You Won!";
-    }
-    else if (num ==0 && randomNum ==2)
-    {
-        document.getElementById('decision').innerHTML = "You Won!";
-    }
-    else if (num == randomNum)
+    if (num==randomNum)
     {
         document.getElementById('decision').innerHTML = "You Tied!";
     }
+    else if (num==2 && randomNum==1)
+    {
+        document.getElementById('decision').innerHTML = "You Won!";
+        pScore++;
+    }
+    else if (num==1 && randomNum==0)
+    {
+        document.getElementById('decision').innerHTML = "You Won!";
+        pScore++;
+    }
+    else if (num==0 && randomNum==2)
+    {
+        document.getElementById('decision').innerHTML = "You Won!";
+        pScore++;
+    }
     else{
         document.getElementById('decision').innerHTML = "You Lost!";
+        cScore++;
     } 
+
+    document.getElementById('playerScore').innerHTML = pScore;
+    document.getElementById('computerScore').innerHTML = cScore;
+    gameOver(pScore,cScore);
 }
 
 function rock()
